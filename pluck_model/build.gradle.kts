e820ae5b08bc2800f25be0c8132c3c0ab71a8a71
Yaroslav Zagumennikov <yzagumennikov1@mail.ru>
2023-04-03 14:58:30 +0500

    Edit

--- pluck_model/build.gradle.kts
+++ pluck_model/build.gradle.kts
@@ -66,7 +66,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.Yarik-Ydavik"
             artifactId = "pluck_model"
-            version = "3.2"
+            version = "3.3"
 
             afterEvaluate {
                 from(components["release"])
